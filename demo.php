95479673cb3ae006c435a01ae9cbcb45498f16a5
scud <alban.escudier@hotmail.fr>
2022-12-12 15:04:46 +0100

    +tableau

--- demo.php
+++ demo.php
@@ -1,13 +1,15 @@
 
 
 <?php
-$prenom = 'marc';
+/* $prenom = 'marc';
 $nom = 'doe';
 $note = 15;
 $note2 = 10;
 $moyenne = ($note + $note2) /2;
 
 echo "bonjour $prenom $nom vous avez eu $moyenne de moyenne";
-
+ */
+$note = [10, 15, 25, 7, 6, 20];
+echo $note[4];
 ?>
 

8a86afacd225ec9775ada015844b5a982475ce37
scud <alban.escudier@hotmail.fr>
2022-12-13 11:06:36 +0100

    if else et switch (conditions)

--- demo.php
+++ demo.php
@@ -35,5 +35,43 @@ echo $eleve ['prenom'] . ' ' . $eleve ['nom']; */
     ]
  ];
  echo $classe [1]['note'][2]; */
+
+ /* $note = readline ('Entrez votre note');
+ if ($note > 10){
+    echo 'vous avez la moyenne';
+ } elseif ($note == 10) {
+    echo 'vous avez juste la moyenne';
+ } else {
+    echo 'vous n\'avez pas la moyenne';
+ } */
+
+ /* $action = (int)readline ('Entrez votre action (1: Attaquez, 2: Defendre, 3: Passer son tour)');
+ if ($action === 1) {
+    echo 'vous attaquez';
+ }elseif ($action === 2){
+    echo 'vous defendez';
+ }elseif ($action === 3){
+    echo 'vous passez votre tour';
+ }else {
+    echo 'commande inconnu';
+ } */
+
+ $action = (int)readline ('Entrez votre action (1: Attaquez, 2: Defendre, 3: Passer son tour)'); 
+switch ($action) {
+    case 1:
+        echo 'vous attaquez';
+        break;
+    case 2:
+        echo 'vous defendez';
+        break;
+    case 3:
+        echo 'vous passez votre tour';
+        break;
+    default :
+        echo 'commande inconnu';
+}
+
+
+
 ?>
 

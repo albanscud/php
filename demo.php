ac308141a0e1a2e80c14b3388b84c9fcef920082
scud <alban.escudier@hotmail.fr>
2022-12-13 10:11:38 +0100

    tableau suite

--- demo.php
+++ demo.php
@@ -9,7 +9,31 @@ $moyenne = ($note + $note2) /2;
 
 echo "bonjour $prenom $nom vous avez eu $moyenne de moyenne";
  */
-$note = [10, 15, 25, 7, 6, 20];
-echo $note[4];
+
+/* $note = [10, 15, 25, 7, 6, 20];
+echo $note[4]; */
+
+/* $eleve = ['marc', 'doe', [10, 11, 12]];
+echo $eleve[2][2]; */
+
+/* $eleve = [
+    'prenom' => 'Marc',
+    'nom' => 'Doe',
+    'notes' => [10, 15, 18]
+];
+echo $eleve ['prenom'] . ' ' . $eleve ['nom']; */
+
+/* $classe = [
+    [
+        'nom' => 'scud',
+        'prenom' => 'jean',
+        'note' => [12, 15, 20]
+    ],[
+        'nom' => 'luzy',
+        'prenom' => 'pierre',
+        'note' => [15, 18, 13]
+    ]
+ ];
+ echo $classe [1]['note'][2]; */
 ?>
 
